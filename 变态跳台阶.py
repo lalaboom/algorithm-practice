d0bf06d65044c8e6bd535abe086cfba2a0c5c9c7
Jelly Qin <34963711+lalaboom@users.noreply.github.com>
2019-07-10 10:53:25 +0800

    supplement

--- 变态跳台阶.py
+++ 变态跳台阶.py
@@ -1,10 +1,10 @@
 # 变态跳台阶
 class Solution:
     def jumpFloorII(self, number):
-        if number < 0:
+        if number <= 0:
             return 0
-        else：
-        return pow(2,number-1)
+        else:
+            return pow(2,number-1)
             
 
 
