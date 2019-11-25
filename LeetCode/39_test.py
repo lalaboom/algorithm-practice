1069a3035400272e808b971a5c8e4576ce1ad5bd
Jelly Qin <34963711+lalaboom@users.noreply.github.com>
2019-11-25 20:38:46 +0800

    supplement

---
+++ LeetCode/39_test.py
@@ -0,0 +1,16 @@
+if not candidates:
+    return []
+n = len(candidates)
+res = []
+candidates.sort()
+def helper(i,tmp,target):
+    if target==0:
+        res.append(tmp)
+        return
+    if i == n or target<candidates[i]:
+        return
+    helper(i,tmp+[candidates[i]],target - candidates[i])
+    helper(i+1,tmp,target)
+
+helper(0,[],target)
+return res
